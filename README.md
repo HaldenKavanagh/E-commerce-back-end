d956f5639477243155c092c42c062c6a443e55ea
Halden Kavanagh <haldenkav@gmail.com>
2024-01-24 21:35:46 -0700

    renamed controllers folder and established get routes

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # E-commerce-back-end
 
 
-WHEN I open API GET routes in Insomnia Core for categories, products, or tags
-THEN the data for each of these routes is displayed in a formatted JSON
 WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
 THEN I am able to successfully create, update, and delete data in my database
 
--- routes/api/get-routes.js
+++ routes/api/get-routes.js
@@ -1,3 +1,74 @@
 const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
+
+// http://localhost:3001/api/get/products
+router.get("/products", async (req, res) => {
+  try {
+    const dbProductData = await Product.findAll();
+    if (!dbProductData) {
+      return res.status(404).json({ message: "Products not found" });
+    }
+    const products = dbProductData.map((productList) =>
+      productList.get({ plain: true })
+    );
+
+    res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// http://localhost:3001/api/get/categories
+router.get("/categories", async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findAll();
+    if (!dbCategoryData) {
+      return res.status(404).json({ message: "Categorys not found" });
+    }
+    const categories = dbCategoryData.map((categoryList) =>
+      categoryList.get({ plain: true })
+    );
+
+    res.status(200).json(categories);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// http://localhost:3001/api/get/tags
+router.get("/tags", async (req, res) => {
+  try {
+    const dbTagData = await Tag.findAll();
+    if (!dbTagData) {
+      return res.status(404).json({ message: "Tags not found" });
+    }
+    const tags = dbTagData.map((tagList) => tagList.get({ plain: true }));
+
+    res.status(200).json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//localhost:3001/api/get/producttags
+http: router.get("/producttags", async (req, res) => {
+  try {
+    const dbProductTagData = await ProductTag.findAll();
+    if (!dbProductTagData) {
+      return res.status(404).json({ message: "Product tags not found" });
+    }
+    const producttags = dbProductTagData.map((productTagList) =>
+      productTagList.get({ plain: true })
+    );
+
+    res.status(200).json(producttags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
