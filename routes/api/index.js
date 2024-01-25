76d32d7813360801d009900ecf7d652de6a439b1
Halden Kavanagh <haldenkav@gmail.com>
2024-01-24 21:08:07 -0700

    established file structure for get routes and got server to run

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const getRoutes = require("./get-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/get", getRoutes);
 
 module.exports = router;
