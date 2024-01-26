28d3f0618b6874c7603f265f56cc86099fce233d
Halden Kavanagh <haldenkav@gmail.com>
2024-01-25 22:55:31 -0700

    established delete routes

--- routes/api/delete-routes.js
+++ routes/api/delete-routes.js
@@ -1,3 +1,65 @@
 const router = require("express").Router();
 
+const { Product, Category, Tag, ProductTag } = require("../../models");
+
+//  http://localhost:3001/api/delete/products/:id
+
+router.delete("/products/:id", (req, res) => {
+  // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
+    .catch((err) => res.json(err));
+});
+
+//  http://localhost:3001/api/delete/categories/:id
+
+router.delete("/categories/:id", (req, res) => {
+  // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+//  http://localhost:3001/api/delete/tags/:id
+
+router.delete("/tags/:id", (req, res) => {
+  // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedId) => {
+      res.json(deletedId);
+    })
+    .catch((err) => res.json(err));
+});
+
+//  http://localhost:3001/api/delete/producttags/:id
+
+router.delete("/producttags/:id", (req, res) => {
+  // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
+  ProductTag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProductId) => {
+      res.json(deletedProductId);
+    })
+    .catch((err) => res.json(err));
+});
+
 module.exports = router;
